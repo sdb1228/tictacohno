5d99f39baf10cf0230b8c7dcf7f5391394d9e8bf
Steven Burnett <sburnett@instructure.com>
2017-08-03 15:34:25 -0600

    more stuff

--- thing.js
+++ thing.js
@@ -1,26 +1,55 @@
 const axios = require('axios');
 
+var boardId = 0;
+
 axios.post('http://tictactoe.inseng.net/games')
  .then(function (response) {
    axios.post(`http://tictactoe.inseng.net/games/${response.data.id}/players`, {
-     name: "rachel",
+     name: "sburnett",
      pair: 1,
    })
      .then(function (game) {
-        axios({
-          method:'post',
-          url:`http://tictactoe.inseng.net/games/${response.data.id}/moves`,
-          headers: {'X-Token': game.data.currentPlayer.secret },
-          data: {
-            board: 0,
-            cell: 4,
-          }
-        })
-         .then(function (turn) {
-           console.log(turn.data)
-         })
+        recursiveLoop(response.data.id, game.data.currentPlayer.secret, game)
      })
  })
  .catch(function (error) {
    console.log(error.message);
  });
+
+
+function recursiveLoop (id, secret, game) {
+  var moveVar = move(game)
+  console.log(game.data)
+  axios({
+    method:'post',
+    url:`http://tictactoe.inseng.net/games/${id}/moves`,
+    headers: {'X-Token': secret },
+    data: {
+      board: moveVar.board,
+      cell: moveVar.cell,
+    }
+  })
+   .then(function (turn) {
+     if(turn.data.state === "inProgress") {
+       recursiveLoop(id, secret, turn)
+     }
+   })
+  .catch(function (error) {
+    console.log("HMMMMMMMMMMMMMMM")
+  })
+}
+
+function move (game) {
+  if (!game.data.nextBoard){
+    return {board: 0, cell: 0}
+  }
+  return {board: game.data.nextBoard, cell: cell(game, board)}
+}
+
+function cell (game) {
+  return 2
+}
+
+function board (game) {
+  //TODO board logic
+}
